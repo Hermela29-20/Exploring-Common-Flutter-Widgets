898e652f2c8b42187a26fdbf82c45c9f3eac012f
Hermela29-20 <hermela2029@gmail.com>
2026-04-03 15:54:28 +0300

    initiaizing assignment 2

---
+++ go_route_demo01/android/app/src/main/kotlin/com/example/go_route_demo01/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.go_route_demo01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ go_route_demo03/android/app/src/main/kotlin/com/example/go_route_demo03/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.go_route_demo03
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
